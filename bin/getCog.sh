a71f915b863eef247572a7cb304af8191dbd5685
Bernhard Pieber <bernhard@pieber.com>
2013-07-28 22:47:24 +0200

    remove Cog build number from VM name

--- bin/getCog.sh
+++ bin/getCog.sh
@@ -3,5 +3,4 @@ VERSION="$1"
 BUILD="$2"
 curl http://www.mirandabanda.org/files/Cog/VM/VM.r$BUILD/Cog.app-$VERSION.$BUILD.tgz -o Cog.$BUILD.tgz
 tar -xf Cog.$BUILD.tgz
-mv Cog.app/ Cog.$BUILD.app
 rm Cog.$BUILD.tgz
