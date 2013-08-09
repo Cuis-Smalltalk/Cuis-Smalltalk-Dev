d9554597459467e6879013a0d1546ac26c37e08e
Bernhard Pieber <bernhard@pieber.com>
2013-08-09 19:49:43 +0200

    update newImage shell script to latest version numbers

--- bin/newImage.sh
+++ bin/newImage.sh
@@ -2,5 +2,5 @@
 NAME="$1"
 mkdir $NAME
 cd $NAME
-../Cuis-Smalltalk-Dev/bin/copyImage.sh $NAME Cuis4.2-1766
-../Cuis-Smalltalk-Dev/bin/getCog.sh 13.28 2749
+../Cuis-Smalltalk-Dev/bin/copyImage.sh $NAME Cuis4.2-1788
+../Cuis-Smalltalk-Dev/bin/getCog.sh 13.30 2761
