3497f82a9e16c82cc25ac05bad0073a9bac6a48d
Bernhard Pieber <bernhard@pieber.com>
2013-08-11 17:47:28 +0200

    Make getCog.sh presumably work on Linux

--- bin/getCog.sh
+++ bin/getCog.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 VERSION="$1"
 BUILD="$2"
-curl http://www.mirandabanda.org/files/Cog/VM/VM.r$BUILD/Cog.app-$VERSION.$BUILD.tgz -o Cog.$BUILD.tgz
+case "$OSTYPE" in
+	darwin*) NAME="Cog.app" ;;
+	*) NAME="coglinux" ;;
+esac
+curl http://www.mirandabanda.org/files/Cog/VM/VM.r$BUILD/$NAME-$VERSION.$BUILD.tgz -o Cog.$BUILD.tgz
 tar -xf Cog.$BUILD.tgz
 rm Cog.$BUILD.tgz
