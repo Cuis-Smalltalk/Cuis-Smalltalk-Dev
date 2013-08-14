23b2cd464eb658ac74353b6a897f921e09b703e2
Bernhard Pieber <bernhard@pieber.com>
2013-08-14 11:05:39 +0200

    Update image version number

--- bin/newImage.sh
+++ bin/newImage.sh
@@ -2,5 +2,5 @@
 NAME="$1"
 mkdir $NAME
 cd $NAME
-../Cuis-Smalltalk-Dev/bin/copyImage.sh $NAME Cuis4.2-1789
+../Cuis-Smalltalk-Dev/bin/copyImage.sh $NAME Cuis4.2-1798
 ../Cuis-Smalltalk-Dev/bin/getCog.sh 13.30 2761
