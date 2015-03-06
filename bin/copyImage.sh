082324034cb4638deecda9923e7e773ac9835d82
hhzl <hannes.hirzel@gmail.com>
2015-03-06 19:50:01 +0000

    2203 -> copyImage.sh

--- bin/copyImage.sh
+++ bin/copyImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NEW="$1"
-cp Cuis-Smalltalk-Dev/Cuis4.2-2198.image .
-cp Cuis-Smalltalk-Dev/Cuis4.2-2198.changes .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2203.image .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2203.changes .
 cp Cuis-Smalltalk-Dev/CuisV4.sources .
 mv Cuis*.image $NEW.image
 mv Cuis*.changes $NEW.changes
