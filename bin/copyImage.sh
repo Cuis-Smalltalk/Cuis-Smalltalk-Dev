1179df0862e4f4412da2d58bf11c948a8c7f5eb1
Juan Vuletich <juan@jvuletich.org>
2015-05-28 07:54:28 -0300

    Make almost all tests pass

--- bin/copyImage.sh
+++ bin/copyImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NEW="$1"
-cp Cuis-Smalltalk-Dev/Cuis4.2-2338.image .
-cp Cuis-Smalltalk-Dev/Cuis4.2-2338.changes .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2343.image .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2343.changes .
 cp Cuis-Smalltalk-Dev/CuisV4.sources .
 mv Cuis*.image $NEW.image
 mv Cuis*.changes $NEW.changes
