c0b12390d8946a1fdf05776637ad8e44a9636cf4
Juan Vuletich <juan@jvuletich.org>
2015-06-09 23:14:55 -0300

    Integrated FileMan w/tweaks. More readeable punctuation glyphs. Updates to packages & misc fixes.

--- bin/copyImage.sh
+++ bin/copyImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NEW="$1"
-cp Cuis-Smalltalk-Dev/Cuis4.2-2346.image .
-cp Cuis-Smalltalk-Dev/Cuis4.2-2346.changes .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2358.image .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2358.changes .
 cp Cuis-Smalltalk-Dev/CuisV4.sources .
 mv Cuis*.image $NEW.image
 mv Cuis*.changes $NEW.changes
