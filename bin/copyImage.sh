27c4d038511fc1e7b0c0d7f0d136727660fe1de4
Juan Vuletich <juan@jvuletich.org>
2015-07-01 11:59:33 -0300

    Fixes and enhancements to Progress, Transcript, Halos, WindowTitle. Resizeable FunctionGraphMorph. Adopt FileMan at least in one place

--- bin/copyImage.sh
+++ bin/copyImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NEW="$1"
-cp Cuis-Smalltalk-Dev/Cuis4.2-2390.image .
-cp Cuis-Smalltalk-Dev/Cuis4.2-2390.changes .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2399.image .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2399.changes .
 cp Cuis-Smalltalk-Dev/CuisV4.sources .
 mv Cuis*.image $NEW.image
 mv Cuis*.changes $NEW.changes
