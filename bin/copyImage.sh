37247a82163c663d5cadeab256017708c0251322
Juan Vuletich <juan@jvuletich.org>
2015-08-16 11:56:57 -0300

    Morphic speedup. Especially BouncingAtomsMorph.

--- bin/copyImage.sh
+++ bin/copyImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NEW="$1"
-cp Cuis-Smalltalk-Dev/Cuis4.2-2449.image .
-cp Cuis-Smalltalk-Dev/Cuis4.2-2449.changes .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2461.image .
+cp Cuis-Smalltalk-Dev/Cuis4.2-2461.changes .
 cp Cuis-Smalltalk-Dev/CuisV4.sources .
 mv Cuis*.image $NEW.image
 mv Cuis*.changes $NEW.changes
