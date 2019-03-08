18941271aa5b24c49c55424f7e56c323a053c753
Juan Vuletich <juan@jvuletich.org>
2019-03-08 09:25:08 -0300

    Add scripts to clone package repos and to pull all repos.

---
+++ clonePackageRepos.sh
@@ -0,0 +1,17 @@
+cd ..
+git clone https://github.com/Cuis-Smalltalk/CodeExamples.git
+git clone https://github.com/Cuis-Smalltalk/Games.git
+git clone https://github.com/Cuis-Smalltalk/AMQP.git
+git clone https://github.com/Cuis-Smalltalk/Calendars.git
+git clone https://github.com/Cuis-Smalltalk/Measures.git
+git clone https://github.com/Cuis-Smalltalk/OSProcess.git
+git clone https://github.com/Cuis-Smalltalk/Numerics.git
+git clone https://github.com/Cuis-Smalltalk/Morphic.git
+git clone https://github.com/Cuis-Smalltalk/firmata.git
+git clone https://github.com/Cuis-Smalltalk/Erudite.git
+git clone https://github.com/Cuis-Smalltalk/VectorGraphics.git
+git clone https://github.com/Cuis-Smalltalk/StyledTextEditor.git
+git clone https://github.com/Cuis-Smalltalk/Parsers.git
+git clone https://github.com/Cuis-Smalltalk/Machine-Learning.git
+git clone https://github.com/Cuis-Smalltalk/GeographicInformationSystems.git
+git clone https://github.com/Cuis-Smalltalk/EnhancedText.git
