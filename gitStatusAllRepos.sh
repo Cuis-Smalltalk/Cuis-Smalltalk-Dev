b09c85444f0294babb623a02ce460f6af1a09cdf
Juan Vuletich <juan@jvuletich.org>
2019-03-20 15:34:17 -0300

    add utility script to check git status on all org repos

---
+++ gitStatusAllRepos.sh
@@ -0,0 +1,67 @@
+cd Cuis-Smalltalk-Dev
+pwd
+git status
+cd ../AMQP
+echo "==============================="
+pwd
+git status
+cd ../Calendars
+echo "==============================="
+pwd
+git status
+cd ../CodeExamples
+echo "==============================="
+pwd
+git status
+cd ../Games
+echo "==============================="
+pwd
+git status
+cd ../Measures
+echo "==============================="
+pwd
+git status
+cd ../OSProcess
+echo "==============================="
+pwd
+git status
+cd ../Numerics
+echo "==============================="
+pwd
+git status
+cd ../Morphic
+echo "==============================="
+pwd
+git status
+cd ../firmata
+echo "==============================="
+pwd
+git status
+cd ../Erudite
+echo "==============================="
+pwd
+git status
+cd ../VectorGraphics
+echo "==============================="
+pwd
+git status
+cd ../StyledTextEditor
+echo "==============================="
+pwd
+git status
+cd ../Parsers
+echo "==============================="
+pwd
+git status
+cd ../Machine-Learning
+echo "==============================="
+pwd
+git status
+cd ../GeographicInformationSystems
+echo "==============================="
+pwd
+git status
+cd ../EnhancedText
+echo "==============================="
+pwd
+git status
