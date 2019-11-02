d803f524d6965be1476dea1fe3bd8767e6061d24
Gastón Caruso <gstn.caruso@gmail.com>
2019-11-02 16:36:05 -0300

    Remove temporal image on linux run tests step

--- .ContinuousIntegrationScripts/runTests.sh
+++ .ContinuousIntegrationScripts/runTests.sh
@@ -6,14 +6,7 @@ IMAGE_FILE="$(ls | grep 'Cuis5.0-[0-9]\+.image')"
 RUN_TESTS_SCRIPT_FILEPATH=".ContinuousIntegrationScripts/runTests.st"
 
 runTestsOnLinux() {
-  TEST_TEMPORAL_DIR=$(mktemp -d)
-  TEST_TEMPORAL_IMG="$TEST_TEMPORAL_DIR/cuis.image"
-
-  cp "$IMAGE_FILE" "$TEST_TEMPORAL_IMG"
-
-  sqcogspur64linuxht/bin/squeak -vm-display-null "$TEST_TEMPORAL_IMG" -s "$RUN_TESTS_SCRIPT_FILEPATH"
-
-  rm -r "$TEST_TEMPORAL_DIR"
+  sqcogspur64linuxht/bin/squeak -vm-display-null "$IMAGE_FILE" -s "$RUN_TESTS_SCRIPT_FILEPATH"
 }
 
 runTestsOnMacOS() {
