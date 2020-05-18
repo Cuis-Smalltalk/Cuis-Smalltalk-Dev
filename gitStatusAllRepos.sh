e4b41946c60284543ee5f8f51b6144a9c73331f0
Juan Vuletich <juan@jvuletich.org>
2020-05-18 17:30:49 -0300

    git comfort tweaks

--- gitStatusAllRepos.sh
+++ gitStatusAllRepos.sh
@@ -5,69 +5,69 @@ git status
 cd ../AMQP
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Calendars
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../CodeExamples
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Games
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Measures
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../OSProcess
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Numerics
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Morphic
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../firmata
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Erudite
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../VectorGraphics
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../StyledTextEditor
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Parsers
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Machine-Learning
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../GeographicInformationSystems
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../EnhancedText
 echo "==============================="
 pwd
-git status
+git status -s
 cd ../Learning-Cuis
 echo "==============================="
 pwd
-git status
+git status -s
 
