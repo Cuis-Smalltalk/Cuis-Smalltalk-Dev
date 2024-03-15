3022328c046670d1f49cddd000f60a6c1ef752d4
Juan Vuletich <juan@jvuletich.org>
2024-03-15 16:58:44 -0300

    Update CI script

--- .ContinuousIntegrationScripts/runTests.sh
+++ .ContinuousIntegrationScripts/runTests.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 IMAGE_FILE="$(ls CuisImage/ | grep 'Cuis6.3-[0-9]\+.image')"
-RUN_TESTS_SCRIPT_FILEPATH=".ContinuousIntegrationScripts/runTests.st"
+RUN_TESTS_SCRIPT_FILEPATH="/home/runner/work/Cuis-Smalltalk-Dev/Cuis-Smalltalk-Dev/.ContinuousIntegrationScripts/runTests.st"
 
 runTestsOnLinux() {
   /home/runner/work/Cuis-Smalltalk-Dev/Cuis-Smalltalk-Dev/sqcogspur64linux/squeak -vm-display-null CuisImage/"$IMAGE_FILE" -s "$RUN_TESTS_SCRIPT_FILEPATH"
