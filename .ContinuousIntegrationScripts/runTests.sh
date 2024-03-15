8ab369a7040928d6b178eeb1a39a299c1cdbcb2b
Juan Vuletich <juan@jvuletich.org>
2024-03-15 18:19:27 -0300

    Update CI script

--- .ContinuousIntegrationScripts/runTests.sh
+++ .ContinuousIntegrationScripts/runTests.sh
@@ -3,15 +3,14 @@
 set -euo pipefail
 
 IMAGE_FILE="$(ls CuisImage/ | grep 'Cuis6.3-[0-9]\+.image')"
-RUN_TESTS_SCRIPT_FILEPATH_LINUX="/home/runner/work/Cuis-Smalltalk-Dev/Cuis-Smalltalk-Dev/.ContinuousIntegrationScripts/runTests.st"
-RUN_TESTS_SCRIPT_FILEPATH_MACOS=".ContinuousIntegrationScripts/runTests.st"
+RUN_TESTS_SCRIPT_FILEPATH=".ContinuousIntegrationScripts/runTests.st"
 
 runTestsOnLinux() {
-  /home/runner/work/Cuis-Smalltalk-Dev/Cuis-Smalltalk-Dev/sqcogspur64linux/squeak -vm-display-null CuisImage/"$IMAGE_FILE" -s "$RUN_TESTS_SCRIPT_FILEPATH_LINUX"
+  /home/runner/work/Cuis-Smalltalk-Dev/Cuis-Smalltalk-Dev/sqcogspur64linux/squeak -vm-display-null CuisImage/"$IMAGE_FILE" -s "$RUN_TESTS_SCRIPT_FILEPATH"
 }
 
 runTestsOnMacOS() {
-  /Applications/Squeak.app/Contents/MacOS/Squeak -headless CuisImage/"$IMAGE_FILE" -s "$RUN_TESTS_SCRIPT_FILEPATH_MACOS"
+  /Applications/Squeak.app/Contents/MacOS/Squeak -headless CuisImage/"$IMAGE_FILE" -s "$RUN_TESTS_SCRIPT_FILEPATH"
 }
 
 case $RUNNER_OS in
