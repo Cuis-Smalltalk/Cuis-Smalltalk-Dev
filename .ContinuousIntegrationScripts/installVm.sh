69fb4db4ef029335e61718bfb2b3f0d74c9bcc3f
Juan Vuletich <juan@jvuletich.org>
2024-06-06 12:20:51 -0300

    Use an updated VM for CI

--- .ContinuousIntegrationScripts/installVm.sh
+++ .ContinuousIntegrationScripts/installVm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-VM_VERSION="202206021410"
+VM_VERSION="202312181441"
 BASE_VM_DOWNLOAD_PATH="https://github.com/OpenSmalltalk/opensmalltalk-vm/releases/download/$VM_VERSION"
 
 echo "Installing VM $VM_VERSION for $RUNNER_OS"
