3da47ee0de2746d123cf45cbe25502978584ac91
Juan Vuletich <juan@jvuletich.org>
2024-07-27 22:16:42 -0300

    Updated images

--- RunCuisOnWindows.bat
+++ RunCuisOnWindows.bat
@@ -1,1 +1,1 @@
-START /B .\CuisVM.app\Contents\Windows-x86_64\Squeak.exe "CuisImage\Cuis7.1-6538.image" -u
+START /B .\CuisVM.app\Contents\Windows-x86_64\Squeak.exe "CuisImage\Cuis7.1-6539.image" -u
