5878f0edc132fd586a5dcdab16a9a00a61293ab0
Eliot Miranda <eliot.miranda@gmail.com>
2025-01-28 12:17:38 -0800

    Don't assume brew is installed in Mac run scripts.

--- RunCuisOnMacTerminal.sh
+++ RunCuisOnMacTerminal.sh
@@ -1,2 +1,4 @@
-export DYLD_LIBRARY_PATH="$(brew --prefix)/lib:${DYLD_LIBRARY_PATH}"
+if which -s brew; then
+	export DYLD_LIBRARY_PATH="$(brew --prefix)/lib:${DYLD_LIBRARY_PATH}"
+fi
 ./CuisVM.app/Contents/MacOS/Squeak CuisImage/Cuis?.?-????.image -u "$@"
