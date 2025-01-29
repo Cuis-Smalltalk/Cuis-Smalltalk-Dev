8204ffa07cec44d2a36de1c7f5deed4580d57288
Juan Vuletich <juan@jvuletich.org>
2025-01-29 15:05:38 -0300

    Updated Images

--- RunCuisOnLinux.sh
+++ RunCuisOnLinux.sh
@@ -41,7 +41,7 @@ else # all-in-one bundle
 
     BINDIR="${ROOT}/CuisVM.app/Contents/Linux-${CPU}/"
     IMAGEDIR="${ROOT}/CuisImage/"
-    IMAGE="${IMAGEDIR}Cuis7.3-7003.image"
+    IMAGE="${IMAGEDIR}Cuis7.3-7036.image"
 fi
 
 VM="${BINDIR}${APP}"
